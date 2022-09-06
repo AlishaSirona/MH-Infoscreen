007abd0b3bc999a57e4d023bbd6f874a780a0af6
AlishaSirona <alisha7sirona@gmail.com>
2022-09-06 14:54:28 +0200

    Added Infoscreen DbContext

---
+++ DbPageLibrary/DbInfoscreenContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbInfoscreenLibrary;
+
+public class DbInfoscreenContext : DbContext
+{
+    private const string connectionString = "server=localhost;port=3306;database=Infoscreen;user=root;password=geheim";
+    readonly ServerVersion serverVersion = MySqlServerVersion.AutoDetect(connectionString);
+
+    public DbSet<Pages> Pages { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}

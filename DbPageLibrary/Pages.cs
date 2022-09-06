1a47674d3ab7d8fbe8cbd31b758c3394cecb0872
AlishaSirona <alisha7sirona@gmail.com>
2022-09-06 14:05:04 +0200

    Create Pages.cs

---
+++ DbPageLibrary/Pages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbPageLibrary;
+
+public class Pages
+{
+    public uint Id { get; set; }
+    public uint Position { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public uint Duration { get; set; }
+    public DateTime StartDate { get; set; } = DateTime.Now.Date;
+    public DateTime EndDate { get; set; } = DateTime.Now.AddDays(10).Date;
+}

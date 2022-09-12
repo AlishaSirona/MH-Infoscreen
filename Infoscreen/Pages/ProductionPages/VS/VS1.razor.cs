ce96c5538c31be68290404a531ed70ed00fcf5aa
AlishaSirona <alisha7sirona@gmail.com>
2022-09-12 13:48:21 +0200

    Versand Layout

---
+++ Infoscreen/Pages/ProductionPages/VS/VS1.razor.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using Infoscreen;
+using Infoscreen.Shared;
+using MudBlazor;
+using Infoscreen.Pages.ProductionPages;
+using Infoscreen.Pages.ProductionPages.ETC;
+using Infoscreen.Pages.ProductionPages.STW;
+using Infoscreen.Pages.ProductionPages.UA;
+using Infoscreen.Pages.ProductionPages.VS;
+using Infoscreen.Pages.ProductionPages.WW;
+using Infoscreen.Data;
+using Radzen;
+using Radzen.Blazor;
+using DbInfoscreenLibrary;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace Infoscreen.Pages.ProductionPages.VS;
+
+public partial class VS1
+{
+}

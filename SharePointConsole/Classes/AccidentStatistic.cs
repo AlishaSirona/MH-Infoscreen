6022e9154ee543fbf4b95f2187a8675f2c97e42e
AlishaSirona <alisha7sirona@gmail.com>
2022-09-13 13:46:44 +0200

    Accident Json

---
+++ SharePointConsole/Classes/AccidentStatistic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointConsole.Classes
+{
+    internal class AccidentStatistic
+    {
+        public DateTime LastAccidentDate { get; set; }
+        public string LastAccidentDepartment { get; set; } = string.Empty;
+
+        public int AccidentFreeDays { get; set; }
+
+        public int AccidentsLastYear { get; set; }
+        public int AccidentsLastYearTillNow { get; set; }
+        public int AccidentsThisYear { get; set; }
+
+        public Dictionary<string, int[]> AccidentsPerDepartment { get; set; } = new Dictionary<string, int[]>();
+
+        public Dictionary<int, int[]> AccidentsPerMonth { get; set; } = new Dictionary<int, int[]>();
+
+
+        public AccidentStatistic()
+        {
+            for (int i = 1; i < 13; i++)
+            {
+                AccidentsPerMonth.Add(i, new int[] { 0, 0 });
+            }
+        }
+
+        public void PrintDebug()
+        {
+            Console.WriteLine($"Last Date {LastAccidentDate}");
+            Console.WriteLine($"Last Dep.: {LastAccidentDepartment}");
+            Console.WriteLine($"Free Days {AccidentFreeDays}");
+            Console.WriteLine($"Last Year {AccidentsLastYear}");
+            Console.WriteLine($"Last Till Now {AccidentsLastYearTillNow}");
+            Console.WriteLine($"This Year {AccidentsThisYear}");
+
+            foreach ( var keyValuePair in AccidentsPerDepartment)
+            {
+                Console.WriteLine($"Key: {keyValuePair.Key} Value: {keyValuePair.Value[0]} {keyValuePair.Value[1]}");
+            }
+
+            foreach (var keyValuePair in AccidentsPerMonth)
+            {
+                Console.WriteLine($"Key: {keyValuePair.Key} Value: {keyValuePair.Value[0]} {keyValuePair.Value[1]}");
+            }
+        }
+    }
+}
--- SharePointConsole/Unfall/AccidentController.cs
+++ SharePointConsole/Unfall/AccidentController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharePointConsole.Classes;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace SharePointConsole.Unfall
 {
@@ -45,11 +48,55 @@ namespace SharePointConsole.Unfall
 
         static void CalcData(List<AccidentData> accidents)
         {
+            AccidentStatistic accidentStatistic = new AccidentStatistic();
 
-            foreach (var item in accidents)
+            accidentStatistic.LastAccidentDate = accidents.Last().AccidentDate;
+            accidentStatistic.LastAccidentDepartment = accidents.Last().Department;
+            accidentStatistic.AccidentFreeDays = (int)(DateTime.Now - accidents.Last().AccidentDate).TotalDays;
+            accidentStatistic.AccidentsLastYear = accidents.Where(item => item.AccidentDate.Year == DateTime.Now.AddYears(-1).Year).ToList().Count;
+            accidentStatistic.AccidentsLastYearTillNow = accidents.Where(item => item.AccidentDate.Year == DateTime.Now.AddYears(-1).Year && item.AccidentDate < DateTime.Now.AddYears(-1)).ToList().Count;
+            accidentStatistic.AccidentsThisYear = accidents.Where(item => item.AccidentDate.Year == DateTime.Now.Year).ToList().Count;
+
+            foreach (var dep in accidents.Select(item => item.Department).Distinct())
             {
-                Console.WriteLine($"{item.AccidentDate} {item.Department}");
+                if (!accidentStatistic.AccidentsPerDepartment.ContainsKey(dep))
+                {
+                    accidentStatistic.AccidentsPerDepartment.Add(dep, new int[] { 0, 0 });
+                }
+
+                accidentStatistic.AccidentsPerDepartment[dep][0] = accidents
+                    .Where(item => item.AccidentDate.Year == DateTime.Now.AddYears(-1).Year && item.Department == dep)
+                    .ToList().Count;
+
+                accidentStatistic.AccidentsPerDepartment[dep][1] = accidents
+                    .Where(item => item.AccidentDate.Year == DateTime.Now.Year && item.Department == dep)
+                    .ToList().Count;
             }
+
+            for (int i = 1; i < 13; i++)
+            {
+                accidentStatistic.AccidentsPerMonth[i][0] = accidents
+                    .Where(item => item.AccidentDate.Year == DateTime.Now.AddYears(-1).Year && item.AccidentDate.Month == i)
+                    .ToList().Count;
+
+                accidentStatistic.AccidentsPerMonth[i][0] = accidents
+                    .Where(item => item.AccidentDate.Year == DateTime.Now.Year && item.AccidentDate.Month == i)
+                    .ToList().Count;
+            }
+
+            SerializData(accidentStatistic);
+        }
+
+        static void SerializData(AccidentStatistic accidentStatistic)
+        {
+            string output = JsonConvert.SerializeObject(accidentStatistic, Formatting.Indented);
+
+            string folderPath = "json";
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            System.IO.File.WriteAllText(Path.Combine(folderPath, "accident.json"), output);
         }
     }
 }

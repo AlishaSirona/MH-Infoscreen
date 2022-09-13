95443e425bc76a6ef275067ed6fed86d129bb4ae
AlishaSirona <alisha7sirona@gmail.com>
2022-09-13 15:52:09 +0200

    Create StwStatistic

---
+++ SharePointConsole/Classes/StwStatistic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointConsole.Classes
+{
+    internal class StwStatistic
+    {
+        public double StundenleistungDay { get; set; }
+        public double StundenleistungWeek { get; set; }
+        public double StundenleistungMonth { get; set; }
+        public double AusbringenDay { get; set; }
+        public double AusbringenWeek { get; set; }
+        public double AusbringenMonth { get; set; }
+        public double PowerOnDay { get; set; }
+        public double PowerOnWeek { get; set; }
+        public double PowerOnMonth { get; set; }
+        public double SchrottDay { get; set; }
+        public double SchrottWeek { get; set; }
+        public double SchrottMonth { get; set; }
+        public double SauerstoffDay { get; set; }
+        public double SauerstoffWeek { get; set; }
+        public double SauerstoffMonth { get; set; }
+        public double PowerOffDay { get; set; }
+        public double PowerOffWeek { get; set; }
+        public double PowerOffMonth { get; set; }
+
+
+        public void PrintDebug()
+        {
+            Console.WriteLine($"Stundenleistung {StundenleistungDay} {StundenleistungMonth} {StundenleistungWeek}");
+            Console.WriteLine($"Ausbringen {AusbringenDay} {AusbringenWeek} {AusbringenMonth}");
+            Console.WriteLine($"Power On {PowerOnDay} {PowerOnWeek} {PowerOnMonth}");
+            Console.WriteLine($"Schrott {SchrottDay} {SchrottWeek} {SchrottMonth}");
+            Console.WriteLine($"Sauerstoff {SauerstoffDay} {SauerstoffWeek} {SauerstoffMonth}");
+            Console.WriteLine($"Power Off {PowerOffDay} {PowerOffWeek} {PowerOffMonth}");
+        }
+    }
+}

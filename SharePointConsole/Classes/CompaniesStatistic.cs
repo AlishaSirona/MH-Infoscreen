6688f870b6271fce617b7684c628595e06755037
AlishaSirona <alisha7sirona@gmail.com>
2022-10-03 14:58:37 +0200

    Create CompaniesStatistic.cs

---
+++ SharePointConsole/Classes/CompaniesStatistic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointConsole.Classes
+{
+    internal class CompaniesStatistic
+    {
+        public string Company { get; set; } = string.Empty;
+        public int EmployeeCount { get; set; }
+        public string Contact { get; set; } = string.Empty;
+        public string Project { get; set; } = string.Empty;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}

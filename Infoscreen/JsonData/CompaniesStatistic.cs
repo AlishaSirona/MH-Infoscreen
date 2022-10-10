f5a838972a5098e514539beeca678df7b6dea268
AlishaSirona <alisha7sirona@gmail.com>
2022-10-10 14:49:39 +0200

    Added Json Classes

---
+++ Infoscreen/JsonData/CompaniesStatistic.cs
@@ -0,0 +1,11 @@
+﻿namespace Infoscreen.JsonData;
+
+public class CompaniesStatistic
+{
+    public string Company { get; set; } = string.Empty;
+    public int EmployeeCount { get; set; }
+    public string Contact { get; set; } = string.Empty;
+    public string Project { get; set; } = string.Empty;
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
---
+++ Infoscreen/JsonData/VisitorStatistic.cs
@@ -0,0 +1,11 @@
+﻿namespace Infoscreen.JsonData;
+
+public class VisitorStatistic
+{
+    public string Title { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public string Firstname { get; set; } = string.Empty;
+    public string Company { get; set; } = string.Empty;
+    public string Contact { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
